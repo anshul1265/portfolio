0b35887f8610b8b70dace29230138a4f77b5064d
Anshul <anshulsingla126@gmail.com>
2022-08-21 21:42:58 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+console.log('Welcome to my portfolio ‎😃');
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.yourisp.com",
+//         Username : "username",
+//         Password : "password",
+//         To : '',
+//         From : "you@isp.com",
+//         Subject : "Portfolio se msg aaya haii",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
